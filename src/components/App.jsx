940d62483dde3f21556d9c7b7661c6131817a248
Marion <mrobert148@gmail.com>
2022-12-08 15:40:46 +0100

    create and render app component

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App() {
+    return <div>Hello world from App component!</div>
+};
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 //1. Create a new React app.
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Hello World!</h1>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
