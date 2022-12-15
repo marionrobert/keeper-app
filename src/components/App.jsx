bc66ecc951b74406d3db436c954c627daa9e0bbf
Marion <mrobert148@gmail.com>
2022-12-15 18:02:53 +0100

    render each note from notes arary with map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+    // notes array needs state bcs it's going to change
+    const [notes, setNotes] = useState([]);
 
     function addNote(note){
-        console.log(note);
+        setNotes((prevNotes) => {
+            return [...prevNotes, note]
+        })
     }
 
     return (
       <div>
         <Heading />
         <CreateArea onAdd={addNote}/>
-        <Note key={1} title="Note title" content="Note content" />
+        { notes.map((note, index) => (
+            <Note key={index} title={note.title} content={note.content} />
+        )
+        )};
         <Footer />
       </div>
     );
